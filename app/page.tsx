12e205b3a510eea804d53bc59ef24417dbbe7d70
Ray Hatfield <ray.hatfield@gmail.com>
2022-11-20 10:41:44 -0600

    add mastodon verification links

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,14 @@ export default function Home() {
         <p>nothing to see here.</p>
         <p>sometimes i <a href="https://twitter.com/rayhatfield">tweet</a>.</p>
         <p>i’m <a rel="me" href="https://mastodon.social/@rayhatfield">on mastodon</a> too.</p>
+        <div className={styles.hidden}>
+          <p>i have these other mastodon accounts too. still trying to figure out which is best.</p>
+          <ul>
+            <li><a rel="me" href="https://hachyderm.io/@raytheist">@raytheist@hachyderm.io</a></li>
+            <li><a rel="me" href="https://mstdn.social/@rayh">@rayh@mstdn.social</a></li>
+            <li><a rel="me" href="https://mastodon.online/@raytheist">@raytheist@mastodon.online</a></li>
+          </ul>
+        </div>
       </div>
     </section>
   )
